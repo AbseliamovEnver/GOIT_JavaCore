07695c6cb74a55fe3b28d69ca037720c490e1435
Enver <a.enver.com@gmail.com>
2017-01-19 22:19:38 +0200

    Module_10_Second_Commit

--- src/main/java/gojava/gojava5/module10/task4/MyExceptionMethodG.java
+++ src/main/java/gojava/gojava5/module10/task4/MyExceptionMethodG.java
@@ -1,6 +1,6 @@
 package gojava.gojava5.module10.task4;
 
-public class MyExceptionMethodG extends Throwable {
+public class MyExceptionMethodG extends Exception {
     public MyExceptionMethodG(String msg) {
         super(msg);
     }
